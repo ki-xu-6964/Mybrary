e919eb5e92aaa072182871589758bc05b3978079
kittykat6964 <kitty@zenbly.io>
2023-03-12 10:56:15 +1100

    Books Create/Index/New

--- server.js
+++ server.js
@@ -3,21 +3,22 @@ if (process.env.NODE_ENV !== 'production') {
 
 }
 
-
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname +'/views')
 app.set('layout', 'layouts/layout' )
 app.use(expressLayouts)
-
-app.use(express.static(__dirname + '../public'))
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10m', extended : false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
@@ -27,5 +28,8 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
 
 app.listen(process.env.PORT || 3000)
